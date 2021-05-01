9c956efd7b2e0e094c09c03eb8792d917dfc6a3e
JoaoALmeida-dev <joao.ranger@gmail.com>
2021-05-01 19:32:53 +0100

    Created CulturaDB.java to hold all the methods specific to our DB THis class is an effort to separate our DB from the default SQL api that can in this way be generalized

--- src/main/java/sql/SqlController.java
+++ src/main/java/sql/SqlController.java
@@ -75,15 +75,7 @@ public class SqlController {
         executeSQL(connection, sql);
     }
 
-    public static void createAllTablesDbCultura(Connection connection) throws SQLException {
-        createTableDb(connection, TABLE_CULTURA_NAME, TABLE_CULTURA);
-        createTableDb(connection, TABLE_ALERTA_NAME, TABLE_ALERTA);
-        createTableDb(connection, TABLE_MEDICAO_NAME, TABLE_MEDICAO);
-        createTableDb(connection, TABLE_PARAMETROCULTURA_NAME, TABLE_PARAMETROCULTURA);
-        createTableDb(connection, TABLE_SENSOR_NAME, TABLE_SENSOR);
-        createTableDb(connection, TABLE_UTILIZADOR_NAME, TABLE_UTILIZADOR);
-        createTableDb(connection, TABLE_ZONA_NAME, TABLE_ZONA);
-    }
+
 
     public static void executeSqlDb(Connection connection, String sql) throws SQLException {
         Statement statement = connection.createStatement();
@@ -186,30 +178,5 @@ public class SqlController {
 */
 
 
-    /**
-     * For testing purposes only
-     */
-    public static void main(String[] args) throws SQLException {
-        Connection connection = connectDb(PATH_DB_USER);
-        ArrayList<Pair> values = new ArrayList<>();
-        values.add(new Pair<>("IdUtilizador", "INT"));
-        values.add(new Pair<>("email", "teste@hotmail.com"));
-        values.add(new Pair<>("password", "1234"));
-        // insertInDbTable(connection,"user",values);
-        ArrayList<String> columns = new ArrayList<>();
 
-        //columns.add("id");
-        //columns.add("nickname");
-        //columns.add("email");
-        //columns.add("password");
-        //selectAllFromDbTable(connection,"user", columns);
-        //selectElementFromDbTable(connection,"user",columns,"nickname","teste");
-        //getElementFromDbTable(connection,"user",columns,"nickname","teste");
-        createAllTablesDbCultura(connection);
-        //System.out.println("values");
-        //System.out.println("columns");
-        //deleteFromDbTable(connection,"user","nickname","teste");
-        //updateFromDbTable(connection,"user",values,"email","teste");
-        //connection.close();
-    }
 }
