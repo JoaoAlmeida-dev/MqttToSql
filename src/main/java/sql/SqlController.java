43d1d8b62787bb3a466dfdeccbbb9375d3171138
JoaoALmeida-dev <joao.ranger@gmail.com>
2021-05-01 19:24:28 +0100

    Moved static objects from SQL Controller to its own static class Sql collums are now static variables

--- src/main/java/sql/SqlController.java
+++ src/main/java/sql/SqlController.java
@@ -1,100 +1,20 @@
 package sql;
-
 import util.Pair;
-
 import java.sql.*;
 import java.util.ArrayList;
 
+import static sql.SqlVariables.*;
 
-public class SqlController{
-
-    private static final String PATH_DB_USER = "C:/Users/pedro/Desktop/culturamysql.db";
-
-    private static final String TABLE_CULTURA_NAME ="cultura";
-    private static final String TABLE_ALERTA_NAME ="alerta";
-    private static final String TABLE_MEDICAO_NAME ="medicao";
-    private static final String TABLE_PARAMETROCULTURA_NAME ="parametrocultura";
-    private static final String TABLE_SENSOR_NAME ="sensor";
-    private static final String TABLE_UTILIZADOR_NAME ="utilizador";
-    private static final String TABLE_ZONA_NAME ="zona";
+public class SqlController {
 
 
-    private static final String[] TABLE_CULTURA = {
-            "\"IdCultura\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE"
-            ,"\"NomeCultura\" TEXT NOT NULL UNIQUE",
-            "\"IdUtilizador\" INTEGER NOT NULL",
-            "\"Estado\" INTEGER NOT NULL",
-            "CONSTRAINT FK_IdUtilizador FOREIGN KEY (IdUtilizador) REFERENCES utilizador(IdUtilizador)"
-    };
-
-
-
-    private static final String[] TABLE_ALERTA = {
-            "\"IdAlerta\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE"
-            ,"\"IdZona\" INTEGER NOT NULL",
-            "\"IdSensor\" INTEGER NOT NULL",
-            "\"Hora\" TEXT NOT NULL",
-            "\"Leitura\" TEXT NOT NULL",
-            "\"TipoAlerta\" TEXT NOT NULL","\"Cultura\" TEXT NOT NULL","\"Mensagem\" TEXT NOT NULL","\"IdUtilizador\" INTEGER NOT NULL",
-            "\"HoraEscrita\" TEXT NOT NULL UNIQUE","\"NivelAlerta\" TEXT NOT NULL","\"IdParametroCultura\" INTEGER NOT NULL"
-            ,"CONSTRAINT FK_IdZona FOREIGN KEY (IdZona) REFERENCES zona(IdZona)"
-            ,"CONSTRAINT FK_IdSensor FOREIGN KEY (IdSensor) REFERENCES sensor(IdSensor)"};
-
-    private static final String[] TABLE_MEDICAO = {
-            "\"IdMedicao\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
-            "\"IdZona\" INTEGER NOT NULL",
-            "\"IdSensor\" INTEGER NOT NULL",
-            "\"Hora\" TEXT NOT NULL",
-            "\"Leitura\" TEXT NOT NULL",
-            "CONSTRAINT FK_IdZona FOREIGN KEY (IdZona) REFERENCES zona(IdZona)",
-            "CONSTRAINT FK_IdSensor FOREIGN KEY (IdSensor) REFERENCES sensor(IdSensor)"};
-
-    private static final String[] TABLE_PARAMETROCULTURA = {
-            "\"IdParametroCultura\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
-            "\"IdCultura\" INTEGER NOT NULL",
-            "\"MinHumidade\" NUMERIC NOT NULL",
-            "\"MaxHumidade\" NUMERIC NOT NULL",
-            "\"MinTemperatura\" NUMERIC NOT NULL",
-            "\"MaxTemperatura\" NUMERIC NOT NULL",
-            "\"MinLuz\" NUMERIC NOT NULL",
-            "\"MaxLuz\" NUMERIC NOT NULL",
-            "\"DangerZoneMinHumidade\" NUMERIC NOT NULL",
-            "\"DangerZoneMaxHumidade\" NUMERIC NOT NULL",
-            "\"DangerZoneMinTemperatura\" NUMERIC NOT NULL",
-            "\"DangerZoneMaxTemperatura\" NUMERIC NOT NULL",
-            "\"DangerZoneMinLuz\" NUMERIC NOT NULL",
-            "\"DangerZoneMaxLuz\" NUMERIC NOT NULL",
-            "CONSTRAINT FK_IdCultura FOREIGN KEY (IdCultura) REFERENCES cultura(IdCultura)"};
-
-    private static final String[] TABLE_SENSOR = {
-            "\"IdSensor\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
-            "\"Tipo\" TEXT NOT NULL",
-            "\"LimiteInferior\" INTEGER NOT NULL",
-            "\"LimiteSuperior\" INTEGER NOT NULL",
-            "\"IdZona\" INTEGER NOT NULL",
-            "CONSTRAINT FK_IdZona FOREIGN KEY (IdZona) REFERENCES zona(IdZona)"
-    };
-    //tablename:utilizador
-    private static final String[] TABLE_UTILIZADOR = {
-            "\"IdUtilizador\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
-            "\"NomeInvestigador\" TEXT NOT NULL",
-            "\"EmailUtilizador\" TEXT NOT NULL",
-            "\"TipoUtilizador\" TEXT NOT NULL"
-    };
-
-    private static final String[] TABLE_ZONA = {
-            "\"IdZona\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
-            "\"Temperatura\" NUMERIC NOT NULL",
-            "\"Humidade\" NUMERIC NOT NULL",
-            "\"Luz\" NUMERIC NOT NULL"
-    };
 
     /**
      * ATTENTION: important to close the connection after using the db
-     * */
+     */
     public static Connection connectDb(String pathToDb) throws SQLException {
         // SQLite connection string
-        String url = "jdbc:sqlite:"+ pathToDb;
+        String url = "jdbc:sqlite:" + pathToDb;
         Connection connection = DriverManager.getConnection(url);
         return connection;
     }
@@ -118,13 +38,13 @@ public class SqlController{
     public static void createTableDb(Connection connection, String tableName, String[] columns) throws SQLException {
 
         /** Usage example each line is a element in the ArrayList columns
-           CREATE TABLE "USER" (
-           "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
-           "nickname" TEXT NOT NULL UNIQUE,
-           "email" TEXT NOT NULL UNIQUE,
-           "password" TEXT NOT NULL
-           );
-           */
+         CREATE TABLE "USER" (
+         "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+         "nickname" TEXT NOT NULL UNIQUE,
+         "email" TEXT NOT NULL UNIQUE,
+         "password" TEXT NOT NULL
+         );
+         */
 
         //CREATE TABLE visitor (
         //        id int NOT NULL,
@@ -143,26 +63,26 @@ public class SqlController{
         //");
 
         String columnsString = "";
-        for(String column: columns){
-            columnsString+=column + ",";
+        for (String column : columns) {
+            columnsString += column + ",";
         }
 
-        columnsString = columnsString.substring(0,columnsString.length()-1);
-        String sql = "CREATE TABLE IF NOT EXISTS "+ "\"" + tableName + "\"" + " ( \n "
+        columnsString = columnsString.substring(0, columnsString.length() - 1);
+        String sql = "CREATE TABLE IF NOT EXISTS " + "\"" + tableName + "\"" + " ( \n "
                 + columnsString
                 + ");";
 
-        executeSQL(connection , sql);
+        executeSQL(connection, sql);
     }
 
     public static void createAllTablesDbCultura(Connection connection) throws SQLException {
-        createTableDb(connection,TABLE_CULTURA_NAME         ,TABLE_CULTURA);
-        createTableDb(connection,TABLE_ALERTA_NAME          ,TABLE_ALERTA);
-        createTableDb(connection,TABLE_MEDICAO_NAME         ,TABLE_MEDICAO);
-        createTableDb(connection,TABLE_PARAMETROCULTURA_NAME,TABLE_PARAMETROCULTURA);
-        createTableDb(connection,TABLE_SENSOR_NAME          ,TABLE_SENSOR);
-        createTableDb(connection,TABLE_UTILIZADOR_NAME      ,TABLE_UTILIZADOR);
-        createTableDb(connection,TABLE_ZONA_NAME            ,TABLE_ZONA);
+        createTableDb(connection, TABLE_CULTURA_NAME, TABLE_CULTURA);
+        createTableDb(connection, TABLE_ALERTA_NAME, TABLE_ALERTA);
+        createTableDb(connection, TABLE_MEDICAO_NAME, TABLE_MEDICAO);
+        createTableDb(connection, TABLE_PARAMETROCULTURA_NAME, TABLE_PARAMETROCULTURA);
+        createTableDb(connection, TABLE_SENSOR_NAME, TABLE_SENSOR);
+        createTableDb(connection, TABLE_UTILIZADOR_NAME, TABLE_UTILIZADOR);
+        createTableDb(connection, TABLE_ZONA_NAME, TABLE_ZONA);
     }
 
     public static void executeSqlDb(Connection connection, String sql) throws SQLException {
@@ -171,27 +91,27 @@ public class SqlController{
     }
 
     public static void insertInDbTable(Connection connection, String tableName, ArrayList<Pair> values) throws SQLException {
-        String columnsString ="IdAlerta,";
-        String valuesString ="INT";
-        for(Pair value: values){
-            columnsString+=value.getA()+",";
-            valuesString+="\""+ value.getB()+"\",";
+        String columnsString = "IdAlerta,";
+        String valuesString = "INT";
+        for (Pair value : values) {
+            columnsString += value.getA() + ",";
+            valuesString += "\"" + value.getB() + "\",";
         }
 
-        columnsString = columnsString.substring(0, columnsString.length()-1);
-        valuesString = valuesString.substring(0, valuesString.length()-1);
+        columnsString = columnsString.substring(0, columnsString.length() - 1);
+        valuesString = valuesString.substring(0, valuesString.length() - 1);
 
-        String sql  ="INSERT INTO "+ tableName+" ("+columnsString+") VALUES ("+valuesString+")";
+        String sql = "INSERT INTO " + tableName + " (" + columnsString + ") VALUES (" + valuesString + ")";
 
         System.out.println(sql);
 
-        executeSQL(connection , sql);
+        executeSQL(connection, sql);
 
 
     }
 
     public static void selectAllFromDbTable(Connection connection, String tableName, ArrayList<String> columns) throws SQLException {
-        String sql = "SELECT * FROM "+tableName;
+        String sql = "SELECT * FROM " + tableName;
 
         ResultSet rs = executeQuerry(connection, sql);
 
@@ -210,25 +130,25 @@ public class SqlController{
     }
 
     public static void selectElementFromDbTable(Connection connection, String tableName, ArrayList<String> columns, String param, String paramValue) throws SQLException {
-        String sql = "SELECT * FROM "+tableName+ " WHERE "+param +" = " +"\""+paramValue+"\"";
+        String sql = "SELECT * FROM " + tableName + " WHERE " + param + " = " + "\"" + paramValue + "\"";
 
         ResultSet rs = executeQuerry(connection, sql);
 
         while (rs.next()) {
-            for(String column: columns){
+            for (String column : columns) {
                 System.out.println(rs.getString(column));
             }
         }
     }
 
     public static ArrayList<String> getElementFromDbTable(Connection connection, String tableName, ArrayList<String> columns, String param, String paramValue) throws SQLException {
-        String sql = "SELECT * FROM "+tableName+ " WHERE "+param +" = " +"\""+paramValue+"\"";
+        String sql = "SELECT * FROM " + tableName + " WHERE " + param + " = " + "\"" + paramValue + "\"";
 
         ResultSet rs = executeQuerry(connection, sql);
 
         ArrayList<String> element = new ArrayList<>();
         while (rs.next()) {
-            for(String column: columns){
+            for (String column : columns) {
                 element.add(rs.getString(column));
             }
         }
@@ -236,28 +156,28 @@ public class SqlController{
         return element;
     }
 
-    public static void deleteFromDbTable(Connection connection,String table ,String param, String paramValue) throws SQLException {
-        String sql = "DELETE FROM " + table+ " WHERE "+param +" = " +"\""+paramValue+"\"";
-        executeSQL(connection , sql);
+    public static void deleteFromDbTable(Connection connection, String table, String param, String paramValue) throws SQLException {
+        String sql = "DELETE FROM " + table + " WHERE " + param + " = " + "\"" + paramValue + "\"";
+        executeSQL(connection, sql);
     }
 
     public static void updateFromDbTable(Connection connection, String table, ArrayList<Pair> valuesToChange, String param, String paramValue) throws SQLException {
 
-        String sql = "UPDATE "+ table+ " SET";
-        for(Pair value: valuesToChange){
-            sql += " "+value.getA() + " = \"" + value.getB() + "\" ,";
+        String sql = "UPDATE " + table + " SET";
+        for (Pair value : valuesToChange) {
+            sql += " " + value.getA() + " = \"" + value.getB() + "\" ,";
         }
-        sql = sql.substring(0, sql.length()-1);
-        sql +="WHERE "+param+" = \""+paramValue+"\"";
+        sql = sql.substring(0, sql.length() - 1);
+        sql += "WHERE " + param + " = \"" + paramValue + "\"";
 
-        executeSQL(connection , sql);
+        executeSQL(connection, sql);
     }
 
     public static void executeSQL(Connection connection, String stattement) throws SQLException {
         Statement statement = connection.createStatement();
         statement.execute(stattement);
 
-        System.out.println("Executed "+ stattement);
+        System.out.println("Executed " + stattement);
     }
 /*
     public static void SP1(){
@@ -266,16 +186,15 @@ public class SqlController{
 */
 
 
-
     /**
-     For testing purposes only
+     * For testing purposes only
      */
     public static void main(String[] args) throws SQLException {
         Connection connection = connectDb(PATH_DB_USER);
         ArrayList<Pair> values = new ArrayList<>();
-        values.add(new Pair<>("IdUtilizador","INT"));
-        values.add(new Pair<>("email","teste@hotmail.com"));
-        values.add(new Pair<>("password","1234"));
+        values.add(new Pair<>("IdUtilizador", "INT"));
+        values.add(new Pair<>("email", "teste@hotmail.com"));
+        values.add(new Pair<>("password", "1234"));
         // insertInDbTable(connection,"user",values);
         ArrayList<String> columns = new ArrayList<>();
 
---
+++ src/main/java/sql/SqlVariables.java
@@ -0,0 +1,97 @@
+package sql;
+
+public class SqlVariables {
+    public static final String PATH_DB_USER = "database/culturamysql.db";
+
+
+    public static final String TABLE_MEDICAO_NAME = "medicao";
+    public static final String[] TABLE_MEDICAO_COLLUMS = {"IdMedicao", "IdZona", "IdSensor", "Hora", "Leitura"};
+    public static final String[] TABLE_MEDICAO = {
+            "\"" + TABLE_MEDICAO_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE", //IdMedicao
+            "\"" + TABLE_MEDICAO_COLLUMS[1] + "\" INTEGER NOT NULL", //IdZona
+            "\"" + TABLE_MEDICAO_COLLUMS[2] + "\" INTEGER NOT NULL", //IdSensor
+            "\"" + TABLE_MEDICAO_COLLUMS[3] + "\" TEXT NOT NULL", //Hora
+            "\"" + TABLE_MEDICAO_COLLUMS[4] + "\" TEXT NOT NULL", //Leitura
+            "CONSTRAINT FK_IdZona FOREIGN KEY (" + TABLE_MEDICAO_COLLUMS[1] + ") REFERENCES zona(" + TABLE_MEDICAO_COLLUMS[1] + ")",
+            "CONSTRAINT FK_IdSensor FOREIGN KEY (" + TABLE_MEDICAO_COLLUMS[2] + ") REFERENCES sensor(" + TABLE_MEDICAO_COLLUMS[2] + ")"};
+
+    public static final String TABLE_PARAMETROCULTURA_NAME = "parametrocultura";
+    public static final String[] TABLE_PARAMETROCULTURA_COLLUMS = {"IdParametroCultura", "IdCultura", "MinHumidade", "MaxHumidade", "MinTemperatura", "MaxTemperatura", "MinLuz", "MaxLuz", "DangerZoneMinHumidade", "DangerZoneMaxHumidade", "DangerZoneMinTemperatura", "DangerZoneMaxTemperatura", "DangerZoneMinLuz", "DangerZoneMaxLuz"};
+    public static final String[] TABLE_PARAMETROCULTURA = {
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",  //IdParametroCultura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[1] + "\" INTEGER NOT NULL", //IdCultura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[2] + "\" NUMERIC NOT NULL", //MinHumidade
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[3] + "\" NUMERIC NOT NULL", //MaxHumidade
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[4] + "\" NUMERIC NOT NULL", //MinTemperatura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[5] + "\" NUMERIC NOT NULL", //MaxTemperatura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[6] + "\" NUMERIC NOT NULL", //MinLuz
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[7] + "\" NUMERIC NOT NULL", //MaxLuz
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[8] + "\" NUMERIC NOT NULL", //DangerZoneMinHumidade
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[9] + "\" NUMERIC NOT NULL", //DangerZoneMaxHumidade
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[10] + "\" NUMERIC NOT NULL", //DangerZoneMinTemperatura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[11] + "\" NUMERIC NOT NULL", //DangerZoneMaxTemperatura
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[12] + "\" NUMERIC NOT NULL", //DangerZoneMinLuz
+            "\"" + TABLE_PARAMETROCULTURA_COLLUMS[13] + "\" NUMERIC NOT NULL", //DangerZoneMaxLuz
+            "CONSTRAINT FK_IdCultura FOREIGN KEY (" + TABLE_PARAMETROCULTURA_COLLUMS[1] + ") REFERENCES cultura(" + TABLE_PARAMETROCULTURA_COLLUMS[1] + ")" +  //IdCUltura
+                    ""};
+
+    public static final String TABLE_SENSOR_NAME = "sensor";
+    public static final String[] TABLE_SENSOR_NAME_COLLUMS = {"IdSensor", "Name", "Tipo", "LimiteInferior", "LimiteSuperior", "IdZona",};
+
+    public static final String[] TABLE_SENSOR = {
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE", //IdSensor
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[1] + "\" TEXT NOT NULL",       //Name
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[2] + "\" TEXT NOT NULL",       //Tipo
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[3] + "\" INTEGER NOT NULL",    //LimiteInferior
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[4] + "\" INTEGER NOT NULL",    //LimiteSuperior
+            "\"" + TABLE_SENSOR_NAME_COLLUMS[5] + "\" INTEGER NOT NULL",    //IdZona
+            "CONSTRAINT FK_IdZona FOREIGN KEY (" + TABLE_SENSOR_NAME_COLLUMS[5] + ") REFERENCES zona(" + TABLE_SENSOR_NAME_COLLUMS[5] + ")" //IdZona
+    };
+    //tablename:utilizador
+    public static final String TABLE_UTILIZADOR_NAME = "utilizador";
+    public static final String[] TABLE_UTILIZADOR_COLLUMS = {"IdUtilizador", "NomeInvestigador", "EmailUtilizador", "TipoUtilizador"};
+    public static final String[] TABLE_UTILIZADOR = {
+            "\"" + TABLE_UTILIZADOR_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",  //IdUtilizador
+            "\"" + TABLE_UTILIZADOR_COLLUMS[1] + "\" TEXT NOT NULL",                                  //NomeInvestigador
+            "\"" + TABLE_UTILIZADOR_COLLUMS[2] + "\" TEXT NOT NULL",                                   //EmailUtilizador
+            "\"" + TABLE_UTILIZADOR_COLLUMS[3] + "\" TEXT NOT NULL"                                     //TipoUtilizador
+    };
+
+    public static final String TABLE_ZONA_NAME = "zona";
+    public static final String[] TABLE_ZONA_COLLUMS = {"IdZona", "Temperatura", "Name", "Humidade", "Luz"};
+    public static final String[] TABLE_ZONA = {
+            "\"" + TABLE_ZONA_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
+            "\"" + TABLE_ZONA_COLLUMS[1] + "\" NUMERIC NOT NULL",
+            "\"" + TABLE_ZONA_COLLUMS[2] + "\" TEXT NOT NULL",
+            "\"" + TABLE_ZONA_COLLUMS[3] + "\" NUMERIC NOT NULL",
+            "\"" + TABLE_ZONA_COLLUMS[4] + "\" NUMERIC NOT NULL"
+    };
+    public static final String TABLE_CULTURA_NAME = "cultura";
+    public static final String TABLE_ALERTA_NAME = "alerta";
+    private static final String[] TABLE_CULTURA_COLLUMS = {"IdCultura", "NomeCultura", "IdUtilizador", "Estado"};
+    public static final String[] TABLE_CULTURA = {
+            "\"" + TABLE_CULTURA_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",
+            "\"" + TABLE_CULTURA_COLLUMS[1] + "\" TEXT NOT NULL UNIQUE",
+            "\"" + TABLE_CULTURA_COLLUMS[2] + "\" INTEGER NOT NULL",
+            "\"" + TABLE_CULTURA_COLLUMS[3] + "\" INTEGER NOT NULL",
+            "CONSTRAINT FK_IdUtilizador FOREIGN KEY (IdUtilizador) REFERENCES utilizador(IdUtilizador)"
+    };
+    private static final String[] TABLE_ALERTA_COLLUMS = {"IdAlerta", "IdZona", "IdSensor", "Hora", "Leitura", "TipoAlerta", "Cultura", "Mensagem", "IdUtilizador", "HoraEscrita", "NivelAlerta", "IdParametroCultura"};
+    public static final String[] TABLE_ALERTA = {
+            "\"" + TABLE_ALERTA_COLLUMS[0] + "\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE",     //IdAlerta
+            "\"" + TABLE_ALERTA_COLLUMS[1] + "\" INTEGER NOT NULL",                                      //IdZona
+            "\"" + TABLE_ALERTA_COLLUMS[2] + "\" INTEGER NOT NULL",                                      //IdSensor
+            "\"" + TABLE_ALERTA_COLLUMS[3] + "\" TEXT NOT NULL",                                         //Hora
+            "\"" + TABLE_ALERTA_COLLUMS[4] + "\" TEXT NOT NULL",                                         //Leitura
+            "\"" + TABLE_ALERTA_COLLUMS[5] + "\" TEXT NOT NULL",                                         //TipoAlerta
+            "\"" + TABLE_ALERTA_COLLUMS[6] + "\" TEXT NOT NULL",                                         //Cultura
+            "\"" + TABLE_ALERTA_COLLUMS[7] + "\" TEXT NOT NULL",                                         //Mensagem
+            "\"" + TABLE_ALERTA_COLLUMS[8] + "\" INTEGER NOT NULL",                                      //IdUtilizador
+            "\"" + TABLE_ALERTA_COLLUMS[9] + "\" TEXT NOT NULL UNIQUE",                                  //HoraEscrita
+            "\"" + TABLE_ALERTA_COLLUMS[10] + "\" TEXT NOT NULL",                                        //NivelAlerta
+            "\"" + TABLE_ALERTA_COLLUMS[11] + "\" INTEGER NOT NULL"                                      //IdParametroCultura
+            , "CONSTRAINT FK_IdZona FOREIGN KEY ( " + TABLE_ALERTA_COLLUMS[1] + ") REFERENCES zona(" + TABLE_ALERTA_COLLUMS[1] + ")"
+            , "CONSTRAINT FK_IdSensor FOREIGN KEY (" + TABLE_ALERTA_COLLUMS[2] + ") REFERENCES sensor(" + TABLE_ALERTA_COLLUMS[2] + ")"};
+
+
+}
