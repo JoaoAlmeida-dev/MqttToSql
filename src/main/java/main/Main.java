f3732013788c61e5468116cbe642770428af678d
rscro-iscte <rscro@iscte-iul.pt>
2021-05-18 19:18:41 +0100

    added way to give predictedvalue to db

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,11 +8,18 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import static mqtt.GeneralMqttVariables.*;
+import static sql.CulturaDB.insertMedicao;
+import static sql.SqlController.connectDb;
+import static sql.SqlController.getLatestEntryFromTable;
+import static sql.variables.GeneralVariables.*;
 
 public class Main {
     public static void main(String[] args) {
+        // MQTTREADER TESTING
+
         try {
             Connection connection = CulturaDB.getLocalConnection();
+            CulturaDB.prepareCulturaDB();
             MQTTReader reader= new MQTTReader(BROKER, CLIENT_ID, PERSISTENCE,connection);
             reader.initialize();
         } catch (SQLException throwables) {
@@ -21,5 +28,48 @@ public class Main {
             e.printStackTrace();
         }
 
+
+
+        /*
+        BACKEND MYSQL TESTING
+         */
+
+        /*
+        try {
+            CulturaDB.prepareCulturaDB();
+
+
+
+            Connection localConnection = connectDb(LOCAL_PATH_DB, ROOTUSERNAME, ROOTPASSWORD);
+
+            String document3 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:55 GMT, Medicao=33.552906794871795}}";
+            insertMedicao(document3,localConnection);
+
+            String document ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:53 GMT, Medicao=17.558906794871795}}";
+            insertMedicao(document,localConnection);
+
+            String document5 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:53 GMT, Medicao=15.558906794871795}}";
+            insertMedicao(document5,localConnection);
+
+            String document6 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:53 GMT, Medicao=12.558906794871795}}";
+            insertMedicao(document6,localConnection);
+
+            String document2 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:54 GMT, Medicao=7.552906794871795}}";
+            insertMedicao(document2,localConnection);
+
+            String document7 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:54 GMT, Medicao=3.552906794871795}}";
+            insertMedicao(document7,localConnection);
+
+            String document4 ="Document{{_id=603819de967bf6020c0922c8, Zona=Z1, Sensor=H1, Data=2021-02-25 at 21:42:53 GMT, Medicao=107.552906794871795}}";
+            insertMedicao(document4,localConnection);
+
+            localConnection.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+         */
     }
 }
--- src/main/java/mqtt/MQTTReader.java
+++ src/main/java/mqtt/MQTTReader.java
@@ -2,10 +2,15 @@ package mqtt;
 
 import org.eclipse.paho.client.mqttv3.*;
 import sql.CulturaDB;
+import util.Pair;
 
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import static mqtt.GeneralMqttVariables.*;
 
@@ -15,6 +20,8 @@ public class MQTTReader implements MqttCallback{
     private MqttConnectOptions connOpts;
     private Connection connection;
 
+    private ArrayList<Pair<Integer,LinkedList<Double>>> lastMedicoes;
+
     public MQTTReader(String broker, String clientID, MqttClientPersistence persistence,Connection connection) throws MqttException {
         sampleClient = new MqttClient(broker, clientID, persistence);
         connOpts = new MqttConnectOptions();
@@ -22,6 +29,7 @@ public class MQTTReader implements MqttCallback{
         connOpts.setCleanSession(true);
         connOpts.setConnectionTimeout(10);
         this.connection = connection;
+        lastMedicoes = new ArrayList<>();
     }
 
     public void initialize() {
@@ -80,12 +88,71 @@ public class MQTTReader implements MqttCallback{
         String receivedData = (String) objectInputStream.readObject();
         System.out.println("Received:"+ receivedData);
         CulturaDB.insertMedicao(receivedData, this.connection);
+
+        if(CulturaDB.getLastMedicao(this.connection).isEmpty()) { return;}
+
+        int collectionId = Integer.parseInt(CulturaDB.getLastMedicao(this.connection).get(1));
+        addCollection(collectionId);
+
+        int indexOfCollection = indexOfCollection(collectionId);
+        //Adding value to list
+        if(lastMedicoes.size() == 100) { lastMedicoes.get(indexOfCollection).getB().removeFirst(); }
+        //TODO MUDAR LAST LEITURA
+        double lastMedicao = CulturaDB.getLastLeitura(this.connection);
+
+        //TODO LAST MEDICAO NAO PODE SER 0
+        if(lastMedicao==0) { return;}
+
+        if(lastMedicoes.get(indexOfCollection).getB().isEmpty()){ lastMedicoes.get(indexOfCollection).getB().add(lastMedicao); }
+        else if(lastMedicoes.get(indexOfCollection).getB().getLast() != lastMedicao && lastMedicao!=0) { lastMedicoes.get(indexOfCollection).getB().add(lastMedicao); }
+
+        if(lastMedicoes.get(indexOfCollection).getB().size()<2) { return; }
+
+        //Calculate change percentage of each medicao
+        ArrayList<Double> medicoesPercentages = new ArrayList<>();
+        for(int i=0; i<lastMedicoes.get(indexOfCollection).getB().size()-1; i++) {
+            double percentageToAdd = (lastMedicoes.get(indexOfCollection).getB().get(i)-lastMedicoes.get(indexOfCollection).getB().get(i+1)) * 100
+                    / lastMedicoes.get(indexOfCollection).getB().get(i+1);
+            medicoesPercentages.add(percentageToAdd);
+        }
+
+        double predictedValue = lastMedicoes.get(indexOfCollection).getB().getLast() + (lastMedicoes.get(indexOfCollection).getB().getLast() * calculateAverage(medicoesPercentages)/100);
+
+        //Predicted Value is sent back to backend to be decided if an alerta is sent or not
+        ArrayList<String> medicaoForPredicted = CulturaDB.getLastMedicao(this.connection);
+        if(!medicaoForPredicted.isEmpty()) {
+            medicaoForPredicted.add(String.valueOf(predictedValue));
+            CulturaDB.checkForAlerta(this.connection,medicaoForPredicted,true);
+        }
+
         //System.out.println("Received:"+mqttMessage);
+    }
+
+    private void addCollection(int collection) {
+        for (Pair col : lastMedicoes) {
+            if((int)col.getA() == collection)
+                return;
+        }
+        lastMedicoes.add(new Pair<>(collection,new LinkedList<>()));
+    }
+
+    private int indexOfCollection (int collection) {
+        for (Pair col : lastMedicoes) {
+            if((int)col.getA() == collection)
+                return lastMedicoes.indexOf(col);
+        }
+        return 0;
+    }
+
+    private double calculateAverage(List<Double> marks) {
+        return marks.stream()
+                .mapToDouble(d -> d)
+                .average()
+                .orElse(0.0);
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
         System.out.println("deliveryComplete\n"+iMqttDeliveryToken);
-
     }
 }
